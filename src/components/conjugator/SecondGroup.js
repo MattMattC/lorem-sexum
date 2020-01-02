c76f0e5bf74fe8f79de96317c917a06e369e17ad
Matthieu Coulon <matthieu.coulon@paygreen.fr>
2020-01-02 11:51:34 +0100

    feat: set the 3 groups of conjugate

---
+++ src/components/conjugator/SecondGroup.js
@@ -0,0 +1,41 @@
+const conjugator = (subject, verb) => {
+    let terminaison = '';
+    const baseVerb = verb.value.substr(0, verb.value.length - 2);
+    switch (subject.subjectType) {
+        case 'nomCommuns':
+        case 'nom':
+            terminaison = 'it';
+            break;
+        case 'pronoms':
+            switch (subject.value) {
+                case 'je':
+                    terminaison = 'is';
+                    break;
+                case 'tu':
+                    terminaison = 'is';
+                    break;
+                case 'il':
+                case 'elle':
+                case 'on':
+                    terminaison = 'it';
+                    break;
+                case 'nous':
+                    terminaison = 'issons';
+                    break;
+                case 'vous':
+                    terminaison = 'issez';
+                    break;
+                case 'ils':
+                case 'elles':
+                    terminaison = 'issent';
+                    break;
+                default:
+                    break;
+            }
+            break;
+        default:
+            break;
+    }
+    return baseVerb + terminaison;
+};
+export default conjugator;
---
+++ src/components/conjugator/index.js
@@ -0,0 +1,5 @@
+import FirstGroup from "./FirstGroup";
+import SecondGroup from "./SecondGroup";
+import ThirdGroup from "./ThirdGroup";
+
+export { FirstGroup, SecondGroup, ThirdGroup };
